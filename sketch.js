21e305fe636f0858ad7473b8d7db1a44b55fd20d
JISHNA <77768080+Jishna2008@users.noreply.github.com>
2021-05-25 10:34:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -50,7 +50,7 @@ async function getBackgroundImg()
 {
 
   // write code to fetch time from API
-  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var response = await fetch("http://worldtimeapi.org/api/timezone/America/Argentina/Salta");
 
   //change the data in JSON format
   var responseJSON = await response.json();
